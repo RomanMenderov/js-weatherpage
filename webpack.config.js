191a7dec1457669ac19d470f8580fa3ef5adaf76
Roman Menderov <vladimirovich3112@mail.ru>
2020-12-06 00:43:48 +0300

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+    environment: { arrowFunction: false },
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};

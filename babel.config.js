6d961bf0662f0699e4f295a895f0e54eb742ace7
Roman Menderov <vladimirovich3112@mail.ru>
2020-12-09 18:04:59 +0300

    fix babel and webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        // "targets": "> 0.25%, not dead",
+        useBuiltIns: "entry", // alternative mode: "entry"/ "usage"
+        corejs: 3, // default would be 2
+      },
+    ],
+  ],
 };
